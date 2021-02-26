e5ee113d9bd8928aebc81600b2cada7047aa0299
Evan Berg <evan.m.berg@gmail.com>
2021-02-25 21:48:45 -0500

    basic routing for user with urls set up. displaying json properly

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,23 @@
+class Api::V1::UsersController < ApplicationController
+  def index
+    @users = User.all
+    data = []
+    @users.each do |user|
+      json_user = user.as_json
+      json_user['urls'] = user.urls
+      data.push(json_user)
+    end
+    respond_to do |f|
+      f.json {render json: {users: data}}
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+    @json_user = @user.as_json
+    @json_user['urls'] = @user.urls
+    respond_to do |f|
+      f.json {render json: {user: @json_user}}
+    end
+  end
+end
