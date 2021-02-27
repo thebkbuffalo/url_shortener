2d78cfbda020ac9d5bad17fa08a4ea465b80c1a3
Evan Berg <evan.m.berg@gmail.com>
2021-02-27 18:16:27 -0500

    added create user endpoint

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -20,4 +20,18 @@ class Api::V1::UsersController < ApplicationController
       f.json {render json: {user: @json_user}}
     end
   end
+
+  def create
+    new_user = User.new(user_params)
+    if new_user.save
+      render json: {new_user: new_user}
+    else
+      render json: {message: 'something went wrong'}
+    end
+  end
+
+  private
+    def user_params
+      params.permit(:name, :email)
+    end
 end
