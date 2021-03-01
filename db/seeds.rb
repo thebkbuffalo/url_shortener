1b88fc30251d0f6f09022cd06d4d901bc79f2dfc
Evan Berg <evan.m.berg@gmail.com>
2021-03-01 14:29:04 -0500

    added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(name: 'Frodo Baggins', email: 'frodo@shire.ca')
+User.create(name: 'Gandalf White', email: 'gandalf@white.wiz')
+
+user = User.find_by(name: 'Frodo Baggins')
+Link.create(user_id: user.id, url: 'https://google.com', slug: 'google')
+Link.create(user_id: user.id, url: 'https://google.com')
+
+user = User.find_by(name: 'Gandalf White')
+Link.create(user_id: user.id, url: 'https://google.com', slug: 'google123')
+Link.create(user_id: user.id, url: 'https://google.com')
